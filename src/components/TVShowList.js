ced2d98a1fe305ec82c6d49f60898a64a6757af4
Prince <avecillasprince@yahoo.com>
2022-11-01 20:36:20 -0400

    Done

--- src/components/TVShowList.js
+++ src/components/TVShowList.js
@@ -1,14 +1,15 @@
 import React from "react";
+import TVShow from "./TVShow"
 import { Grid } from "semantic-ui-react";
 
 function TVShowList(props) {
   function mapAllShows() {
     if (!!props.searchTerm) {
-      props.shows.map((s) => {
-        if (s.name.toLowerCase().includes(props.searchTerm)) {
-          <TVShow show={s} key={s.id} selectShow={props.selectShow} />;
-        }
-      });
+      return props.shows.filter((s) => {
+        return s.name.toLowerCase().includes(props.searchTerm)
+      }).map(s => {
+        return <TVShow show={s} key={s.id} selectShow={props.selectShow} />;
+      })
     }
     return props.shows.map((s) => (
       <TVShow show={s} key={s.id} selectShow={props.selectShow} />
